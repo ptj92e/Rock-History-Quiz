8150cc20fccfc3adba7bc8550dc75080b50b196e
Phillip Jones <phillipjones@Amandas-MacBook-Pro.local>
2019-11-12 09:56:24 -0600

    Updated questions

---
+++ questions.js
@@ -0,0 +1,27 @@
+let questions = [
+    {
+        title: "What is the most popular guitar brand in rock music?",
+        choices: ["Fender", "Gibson", "Ibanez", "Dean"],
+        answer: "Gibson",
+    },
+    {
+        title: "Which band performed the song Thunderstruck?",
+        choices: ["AC/DC", "Guns N' Roses", "The Eagles", "Metallica"],
+        answer: "AC/DC",
+    },
+    {
+        title: "What is the name of the drummer from the band Rush?",
+        choices: ["Dave Grohl", "John Bonham", "Neil Peart", "Chad Smith"],
+        answer: "Neil Peart",
+    },
+    {
+        title: "Slash is the guitar player for which band?",
+        choices: ["Led Zeppelin", "Aerosmith", "Queen", "Guns N' Roses"],
+        answer: "Guns N' Roses",
+    },
+    {
+        title: "Which band was considered the first metal band?",
+        choices: ["Metallica", "Black Sabbath", "Iron Maiden", "Dream Theater"],
+        answer: "Black Sabbath",
+    },
+]
