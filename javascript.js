61cb52519f6cd66e3e3e0dc8eac27444e5000194
Phillip Jones <phillipjones@Amandas-MacBook-Pro.local>
2019-11-13 08:29:18 -0600

    Added buttons

--- javascript.js
+++ javascript.js
@@ -3,6 +3,8 @@ let questNumEl = document.getElementById("questNum");
 let questTextEl = document.getElementById("questText");
 let buttonsEl = document.getElementById("buttons");
 let startQuizBtn = document.getElementById("startQuiz");
+let buttonsAB = document.getElementById("answerAB");
+let buttonsCD = document.getElementById("answerCD");
 
 let secondsLeft = 80;
 
@@ -17,7 +19,29 @@ function startQuiz() {
             clearInterval(timerInterval);
         }
    }, 1000);
+   startQuizBtn.remove();
+   createEl();
 }
 
+function createEl() {
+    let buttonA = document.createElement("button");
+    let buttonB = document.createElement("button");
+    let buttonC = document.createElement("button");
+    let buttonD = document.createElement("button");
 
+    buttonA.setAttribute("class", "btn btn-light col-4 m-3");
+    buttonB.setAttribute("class", "btn btn-light col-4 m-3");
+    buttonC.setAttribute("class", "btn btn-light col-4 m-3");  
+    buttonD.setAttribute("class", "btn btn-light col-4 m-3");  
+
+    buttonA.setAttribute("id", "buttonA");
+    buttonB.setAttribute("id", "buttonB");
+    buttonC.setAttribute("id", "buttonC");  
+    buttonD.setAttribute("id", "buttonD");  
+
+    buttonsAB.appendChild(buttonA);
+    buttonsAB.appendChild(buttonB);
+    buttonsCD.appendChild(buttonC);
+    buttonsCD.appendChild(buttonD);
+}
 
