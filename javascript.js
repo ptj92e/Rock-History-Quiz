20074c8666083cb15f05e0977105428e2ddfbb7b
Phillip Jones <phillipjones@Amandas-MBP.attlocal.net>
2019-11-13 19:53:28 -0600

    Updated Javascript

--- javascript.js
+++ javascript.js
@@ -1,16 +1,19 @@
+//Body Variables
 let timeEl = document.getElementById("timeLeft");
 let questNumEl = document.getElementById("questNum");
 let questTextEl = document.getElementById("questText");
 let buttonsEl = document.getElementById("buttons");
 let startQuizBtn = document.getElementById("startQuiz");
 let answersDiv = document.getElementById("answers");
-
+//Question Variables
 let questTitle = questions[0].title;
-
+let questChoices = questions[0].choices;
+let questAnswer = questions[0].answer;
+//Timer Variables
 let secondsLeft = 80;
-
+//Start Button
 startQuizBtn.addEventListener("click", startQuiz);
-
+//Start Quiz Function
 function startQuiz() {
     let timerInterval = setInterval(function () {
         timeEl.textContent = secondsLeft;
@@ -23,8 +26,9 @@ function startQuiz() {
    startQuizBtn.remove();
    createEl();
    textContent();
+   answerChoice();
 }
-
+//Creating and Appending Questions and Answers
 function createEl() {
     let buttonA = document.createElement("button");
     let buttonB = document.createElement("button");
@@ -41,19 +45,37 @@ function createEl() {
     buttonC.setAttribute("id", "buttonC");  
     buttonD.setAttribute("id", "buttonD");  
 
+    buttonA.setAttribute("data-answer", questChoices[0]);
+    buttonB.setAttribute("data-answer", questChoices[1]);
+    buttonC.setAttribute("data-answer", questChoices[2]);  
+    buttonD.setAttribute("data-answer", questChoices[3]);  
+
     answersDiv.appendChild(buttonA);
     answersDiv.appendChild(buttonB);
     answersDiv.appendChild(buttonC);
     answersDiv.appendChild(buttonD);
 }
-
+//Circulating Between Questions
 function textContent() {
     questTextEl.textContent = questTitle;
-    questions[0].choices.forEach(function(choice) {
-        buttonA.textContent = questions[0].choices[0];
-        buttonB.textContent = questions[0].choices[1];
-        buttonC.textContent = questions[0].choices[2];
-        buttonD.textContent = questions[0].choices[3];
+    questChoices.forEach(function(event) {
+        buttonA.textContent = questChoices[0];
+        buttonB.textContent = questChoices[1];
+        buttonC.textContent = questChoices[2];
+        buttonD.textContent = questChoices[3];
     })
 }
+//Click Listeners for Answer
+answersDiv.addEventListener("click", function(event) {
+    if(event.target.matches("button")) {
 
+    }
+})
+
+function answerChoice() {
+    if (buttonsEl.getAttribute("data-answer") === questAnswer) {
+        questions++;
+    } else {
+        secondsLeft - 10;
+    }
+}
